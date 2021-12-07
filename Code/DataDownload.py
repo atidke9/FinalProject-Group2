1507e98d5eefd930ad4d33a86d54d05b61ece7c4
atidke9 <80483562+atidke9@users.noreply.github.com>
2021-12-06 19:13:48 -0500

    Update DataDownload.py

--- Code/DataDownload.py
+++ Code/DataDownload.py
@@ -1,8 +1,8 @@
 import os
 from zipfile import ZipFile
 
+os.system('pip install kaggle')
 os.system('kaggle datasets download ilhamfp31/yelp-review-dataset' )
-os.system('unzip yelp-review-dataset.zip')
 
 # specifying the zip file name
 file_name = "yelp-review-dataset.zip"
